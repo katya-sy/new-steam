6466ee2a66fe846d6f7cf15488a273f2438bedd2
katya-sy <katya_sy_28@mail.ru>
2024-10-18 09:15:23 +0900

    fix game dialog

--- components/profile-sidebar.tsx
+++ components/profile-sidebar.tsx
@@ -85,12 +85,14 @@ export const ProfileSidebar = () => {
               </Dialog.Trigger>
               <Dialog.Portal>
                 <Dialog.Overlay className="absolute inset-0 bg-black/70 w-screen" />
-                <Dialog.Content className="top-1/2 left-1/2 fixed flex flex-col gap-12 bg-bg p-5 rounded-lg w-[calc(100%-32px)] sm:w-[500px] -translate-x-1/2 -translate-y-1/2">
-                  <Dialog.Title className="font-medium text-2xl text-white">
-                    Добавление игры
-                  </Dialog.Title>
+                <Dialog.Content className="top-1/2 left-1/2 fixed bg-bg p-5 rounded-lg w-[calc(100%-32px)] sm:w-[500px] -translate-x-1/2 -translate-y-1/2">
                   <Dialog.Description />
-                  <AddGameForm />
+                  <div className="flex flex-col gap-12">
+                    <Dialog.Title className="font-medium text-2xl text-white">
+                      Добавление игры
+                    </Dialog.Title>
+                    <AddGameForm />
+                  </div>
                   <Dialog.Close className="top-2 right-2 absolute">
                     <Close />
                   </Dialog.Close>
