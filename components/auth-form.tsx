ae597cacf2a5bf9414ebfa1b0ae368cc60b4c099
The73756 <the73756the@gmail.com>
2025-02-23 17:41:46 +0900

    add logout

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -1,93 +1,100 @@
 "use client";
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Button } from '@/components/ui/button'
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { getProfile, login } from '@/api/user-api'
-import { toast } from "sonner"
-import { setCookie } from '@/lib/cookie'
-import { useUserStore } from '@/store/user-store'
-import {ErrorToast} from "@/components/error-toast";
-import {Dispatch, SetStateAction} from "react";
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { getProfile, login } from "@/api/user-api";
+import { toast } from "sonner";
+import { setCookie } from "@/lib/cookie";
+import { useUserStore } from "@/store/user-store";
+import { ErrorToast } from "@/components/error-toast";
+import { Dispatch, SetStateAction } from "react";
 
 const formSchema = z.object({
-  username: z.string().min(1, 'Обязательное поле'),
+  username: z.string().min(1, "Обязательное поле"),
   password: z
     .string()
-    .min(1, 'Обязательное поле')
+    .min(1, "Обязательное поле")
     .min(3, {
-      message: 'Короткий пароль',
+      message: "Короткий пароль",
     })
     .max(24, {
-      message: 'Длинный пароль',
+      message: "Длинный пароль",
     }),
-})
+});
 
-export const AuthForm = ({setOpen}: {setOpen: Dispatch<SetStateAction<boolean>>}) => {
+export const AuthForm = ({
+  setOpen,
+}: {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     },
-  })
-  const setProfile = useUserStore((state) => state.setProfile)
+  });
+  const setProfile = useUserStore((state) => state.setProfile);
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    const { success, data, error } = await login(values)
+    const { success, data, error } = await login(values);
     if (success) {
-      form.reset()
-      await setCookie('token', String(data?.key))
+      form.reset();
+      await setCookie("token", String(data?.key));
 
-      const profileRes = await getProfile()
-      toast(`Добро пожаловать, ${profileRes.data?.user.username}!`)
-      setProfile(profileRes.data)
+      const profileRes = await getProfile();
+      toast(`Успешно вошли как ${profileRes.data?.user.username}`);
+      setProfile(profileRes.data);
 
-      setOpen(false)
+      setOpen(false);
     } else {
-      toast(
-        <ErrorToast error={error}/>
-      )
+      toast(<ErrorToast error={error} />);
     }
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-3">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-3"
+      >
         <FormField
           control={form.control}
           name="username"
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
               <FormControl>
                 <Input placeholder="Логин" {...field} />
               </FormControl>
-              <FormMessage/>
+              <FormMessage />
             </FormItem>
           )}
         />
         <FormField
           control={form.control}
           name="password"
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
               <FormControl>
                 <Input type="password" placeholder="Пароль" {...field} />
               </FormControl>
-              <FormMessage/>
+              <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit" size="sm">Войти</Button>
+        <Button type="submit" size="sm">
+          Войти
+        </Button>
       </form>
     </Form>
-  )
-}
+  );
+};
--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Input } from "./ui/input";
@@ -7,10 +7,28 @@ import * as Tabs from "@radix-ui/react-tabs";
 import { AuthForm } from "./auth-form";
 import { RegForm } from "./reg-form";
 import { DialogPortal } from "./ui/dialog-portal";
-import {useState} from "react";
+import { useState } from "react";
+import { useUserStore } from "@/store/user-store";
+import { toast } from "sonner";
+import { ErrorToast } from "@/components/error-toast";
+import { deleteCookie } from "@/lib/cookie";
+import { logout } from "@/api/user-api";
 
 export const Header = () => {
   const [open, setOpen] = useState(false);
+  const profile = useUserStore((state) => state.profile);
+  const setProfile = useUserStore((state) => state.setProfile);
+
+  const logoutHandler = async () => {
+    const { success, data, error } = await logout();
+    if (success) {
+      toast(`${data?.detail}`);
+      await deleteCookie("token");
+      setProfile(null);
+    } else {
+      toast(<ErrorToast error={error} />);
+    }
+  };
 
   return (
     <header className="border-b-2 border-blue">
@@ -20,38 +38,52 @@ export const Header = () => {
             <Image src="/logo.svg" width={42} height={42} alt="Logo" />
           </Link>
           <div className="flex items-center gap-5">
-            <Link className="font-medium text-blue" href="/user">
-              Личный кабинет
-            </Link>
-            <Dialog.Root open={open} onOpenChange={setOpen}>
-              <Dialog.Trigger className="font-medium text-blue">
-                Войти
-              </Dialog.Trigger>
-              <DialogPortal>
-                <Tabs.Root className="flex flex-col gap-12" defaultValue="auth">
-                  <Tabs.List className="flex items-baseline gap-10">
-                    <Tabs.Trigger
-                      className="font-medium text-white/60 text-xl dialog-tabs-trigger"
-                      value="auth"
-                    >
-                      <Dialog.Title>Вход</Dialog.Title>
-                    </Tabs.Trigger>
-                    <Tabs.Trigger
-                      className="font-medium text-white/60 text-xl dialog-tabs-trigger"
-                      value="reg"
-                    >
-                      <Dialog.Title>Регистрация</Dialog.Title>
-                    </Tabs.Trigger>
-                  </Tabs.List>
-                  <Tabs.Content value="auth">
-                    <AuthForm setOpen={setOpen} />
-                  </Tabs.Content>
-                  <Tabs.Content value="reg">
-                    <RegForm />
-                  </Tabs.Content>
-                </Tabs.Root>
-              </DialogPortal>
-            </Dialog.Root>
+            {profile ? (
+              <>
+                <Link className="font-medium text-blue" href="/user">
+                  Личный кабинет
+                </Link>
+                <button
+                  onClick={logoutHandler}
+                  className="font-medium text-red-500"
+                >
+                  Выйти
+                </button>
+              </>
+            ) : (
+              <Dialog.Root open={open} onOpenChange={setOpen}>
+                <Dialog.Trigger className="font-medium text-blue">
+                  Войти
+                </Dialog.Trigger>
+                <DialogPortal>
+                  <Tabs.Root
+                    className="flex flex-col gap-12"
+                    defaultValue="auth"
+                  >
+                    <Tabs.List className="flex items-baseline gap-10">
+                      <Tabs.Trigger
+                        className="font-medium text-white/60 text-xl dialog-tabs-trigger"
+                        value="auth"
+                      >
+                        <Dialog.Title>Вход</Dialog.Title>
+                      </Tabs.Trigger>
+                      <Tabs.Trigger
+                        className="font-medium text-white/60 text-xl dialog-tabs-trigger"
+                        value="reg"
+                      >
+                        <Dialog.Title>Регистрация</Dialog.Title>
+                      </Tabs.Trigger>
+                    </Tabs.List>
+                    <Tabs.Content value="auth">
+                      <AuthForm setOpen={setOpen} />
+                    </Tabs.Content>
+                    <Tabs.Content value="reg">
+                      <RegForm />
+                    </Tabs.Content>
+                  </Tabs.Root>
+                </DialogPortal>
+              </Dialog.Root>
+            )}
           </div>
         </div>
         <div className="w-full md:w-1/2">
