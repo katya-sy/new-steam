0c68359aebed0b7380af86cd81c3853ef39a24e7
The73756 <the73756the@gmail.com>
2025-03-26 16:57:50 +0900

    fix sidebar

--- components/filter-sidebar.tsx
+++ components/filter-sidebar.tsx
@@ -7,9 +7,6 @@ export const FilterSidebar = () => {
   const tags = useTagStore((state) => state.tags);
   const setSearchTags = useGameStore((state) => state.setSearchTags);
   const searchTags = useGameStore((state) => state.searchTags);
-  const search = useGameStore((state) => state.search);
-  const setShowAllGames = useGameStore((state) => state.setShowAllGames);
-  const showAllGames = useGameStore((state) => state.showAllGames);
 
   const handleTagToggle = (selectedTags: string[]) => {
     setSearchTags(selectedTags);
@@ -18,14 +15,6 @@ export const FilterSidebar = () => {
   return (
     <div className="flex flex-col gap-5 md:gap-10 py-5 md:py-12 pr-5">
       <div className="flex flex-col gap-2 md:gap-5">
-        {!(searchTags.length !== 0 || search) && (
-          <div
-            onClick={() => setShowAllGames(!showAllGames)}
-            className="mb-3 font-medium text-blue cursor-pointer"
-          >
-            {showAllGames ? "Смотреть лучшие игры" : "Смотреть все игры"}
-          </div>
-        )}
         <h3 className="font-medium text-lg md:text-xl">Популярные теги</h3>
         <ToggleGroup
           type="multiple"
--- components/sidebar-wrapper.tsx
+++ components/sidebar-wrapper.tsx
@@ -3,9 +3,14 @@ import { useEffect, useState } from "react";
 import { FilterSidebar } from "./filter-sidebar";
 import { Tag } from "@/types/game-type";
 import { useTagStore } from "@/store/tag-store";
+import { useGameStore } from "@/store/game-store";
 
 export const SidebarWrapper = ({ data }: { data: Tag[] | null }) => {
   const setTags = useTagStore((state) => state.setTags);
+  const search = useGameStore((state) => state.search);
+  const setShowAllGames = useGameStore((state) => state.setShowAllGames);
+  const showAllGames = useGameStore((state) => state.showAllGames);
+  const searchTags = useGameStore((state) => state.searchTags);
   const [open, setOpen] = useState(window.innerWidth > 768);
 
   useEffect(() => {
@@ -14,14 +19,20 @@ export const SidebarWrapper = ({ data }: { data: Tag[] | null }) => {
 
   return (
     <div>
-      {window.innerWidth < 768 && (
-        <button
-          className="mt-5 font-medium text-blue"
-          onClick={() => setOpen(!open)}
+      {!(searchTags.length !== 0 || search) && (
+        <div
+          onClick={() => setShowAllGames(!showAllGames)}
+          className="mb-3 mt-5 md:mt-12 md:-mb-3 font-medium text-blue cursor-pointer"
         >
-          {open ? "Скрыть" : "Поиск по тегам"}
-        </button>
+          {showAllGames ? "Смотреть лучшие игры" : "Смотреть все игры"}
+        </div>
       )}
+      <button
+        className={`${open ? "text-blue/70" : "text-blue"} font-medium md:hidden`}
+        onClick={() => setOpen(!open)}
+      >
+        {open ? "Скрыть теги" : "Поиск по тегам"}
+      </button>
       {open && <FilterSidebar />}
     </div>
   );
