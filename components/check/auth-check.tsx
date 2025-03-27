f16a7c625d61f71b144f4fa660cf794e721e20b9
The73756 <the73756the@gmail.com>
2025-03-27 17:39:40 +0900

    fix auth check

--- components/check/auth-check.tsx
+++ components/check/auth-check.tsx
@@ -1,40 +1,89 @@
-'use client'
+"use client";
 
-import { ReactNode, useEffect, useState } from 'react'
-import { useUserStore } from '@/store/user-store'
-import { getCookie } from '@/lib/cookie'
-import { getProfile } from '@/api/user-api'
-import { Loader } from '@/components/ui/loader'
+import { ReactNode, useEffect, useState } from "react";
+import { useUserStore } from "@/store/user-store";
+import { getCookie } from "@/lib/cookie";
+import { getProfile } from "@/api/user-api";
+import { Loader } from "@/components/ui/loader";
+import { useRouter, usePathname } from "next/navigation";
+import { toast } from "sonner";
+import { ErrorToast } from "@/components/error-toast";
+import { AUTH_REQUIRED_PATHS, STAFF_ONLY_PATHS } from "@/lib/consts";
 
 interface Props {
-  children: ReactNode
+  children: ReactNode;
 }
 
 export const AuthCheck = ({ children }: Props) => {
-  const profile = useUserStore((state) => state.profile)
-  const setProfile = useUserStore((state) => state.setProfile)
-  const [loading, setLoading] = useState(true)
+  const profile = useUserStore((state) => state.profile);
+  const setProfile = useUserStore((state) => state.setProfile);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     const checkAuth = async () => {
-      if (!profile) {
-        const token = await getCookie('token')
-        if (token) {
-          const { success, data } = await getProfile()
-          if (success) {
-            setProfile(data)
-          }
+      const token = await getCookie("token");
+      const isAuthRequired = AUTH_REQUIRED_PATHS.some((path) =>
+        pathname.startsWith(path),
+      );
+      const isStaffRequired = STAFF_ONLY_PATHS.some((path) =>
+        pathname.startsWith(path),
+      );
+
+      if (isAuthRequired && !token) {
+        router.push(`/`);
+        toast(
+          <ErrorToast
+            error={{
+              message: "Авторизуйтесь для доступа к этой странице",
+              statusCode: 401,
+            }}
+          />,
+        );
+        return;
+      }
+
+      if (token && !profile) {
+        const { success, data } = await getProfile();
+        if (success) {
+          setProfile(data);
+        } else {
+          router.push("/");
+          toast(
+            <ErrorToast
+              error={{
+                message: "Авторизуйтесь для доступа к этой странице",
+                statusCode: 401,
+              }}
+            />,
+          );
+          return;
         }
       }
-      setLoading(false)
-    }
 
-    checkAuth()
-  }, [setProfile, profile])
+      if (isStaffRequired && !profile?.user.is_staff) {
+        router.push("/");
+        toast(
+          <ErrorToast
+            error={{
+              message: "У Вас нет прав администратора",
+              statusCode: 403,
+            }}
+          />,
+        );
+        return;
+      }
+
+      setLoading(false);
+    };
+
+    checkAuth();
+  }, [profile, setProfile, router, pathname]);
 
   if (loading) {
-    return <Loader />
-  } else {
-    return <>{children}</>
+    return <Loader />;
   }
-}
+
+  return <>{children}</>;
+};
--- lib/consts.ts
+++ lib/consts.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const AUTH_REQUIRED_PATHS = ["/profile"];
+export const STAFF_ONLY_PATHS = ["/admin"];
